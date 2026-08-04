e49e711ccf1c1fb09f7fa76b2375d9713fc71bfe
Shra1kumar25 <sra1kumar.e@gmail.com>
2026-08-04 17:25:42 +0000

    JWT authentication added

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost/employee_db"
+DATABASE_URL = "postgresql://postgres:admin123@postgres-db:5432/employee_db"
 
 engine = create_engine(DATABASE_URL)
 
