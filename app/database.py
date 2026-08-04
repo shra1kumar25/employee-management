7d5f83314e1a1d1826b0edc43e541910e014bd8e
Shra1kumar25 <sra1kumar.e@gmail.com>
2026-08-04 18:12:21 +0000

    Use environment variable for database URL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:admin123@postgres-db:5432/employee_db"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:admin123@localhost:5432/employee_db"
+)
+
 
 engine = create_engine(DATABASE_URL)
 
@@ -10,3 +16,5 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
