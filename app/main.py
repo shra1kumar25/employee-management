0c01680f5566993619f9836cd09c18ac937d1ae7
Shra1kumar25 <sra1kumar.e@gmail.com>
2026-08-04 16:58:44 +0000

    Employee CRUD completed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from app.database import engine
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
+
+
 
 app = FastAPI()
 
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Employee(Base):
+
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    department = Column(String)
